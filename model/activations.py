bcf6f97ec29044d703526f12c14cd8371b0a36dd
DKoflerGIT <74835806+DKoflerGIT@users.noreply.github.com>
2022-10-21 12:11:53 +0200

    activations

--- model/activations.py
+++ model/activations.py
@@ -1,16 +1,21 @@
 import numpy as np
 
-def ReLu(v):
-    return np.maximum(0.0, v)
+def ReLu(v, derivative = False):
+    if not derivative: return np.maximum(0.0, v)
+    else: return 0
 
-def Identity(v):
-    return v
+def Identity(v, derivative = False):
+    if not derivative: return v
+    else: return 1
     
-def Sigmoid(v):
-    return 1.0 / (1.0 + np.exp(-v))
+def Sigmoid(v, derivative = False):
+    if not derivative: return 1.0 / (1.0 + np.exp(-v))
+    else: return 0
 
-def Tanh(v):
-    return np.tanh(v)
+def Tanh(v, derivative = False):
+    if not derivative: return np.tanh(v)
+    else: return 0
 
-def Softmax(v):
-    return np.exp(v) / np.sum(np.exp(v))
+def Softmax(v, derivative = False):
+    if not derivative: return np.exp(v) / np.sum(np.exp(v))
+    else: return 0
