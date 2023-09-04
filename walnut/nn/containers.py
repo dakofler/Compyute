087878a9ddb3bc1c65de272ea8516c8c2b3ad32c
DKoflerGIT <74835806+DKoflerGIT@users.noreply.github.com>
2023-09-04 19:35:10 +0200

    small cleanups, blocks module

--- walnut/nn/containers.py
+++ walnut/nn/containers.py
@@ -2,14 +2,13 @@
 
 from walnut.tensor import Tensor, NpArrayLike
 from walnut.nn.module import Module
-from walnut.nn.layers import Linear, Recurrent
 
 
-__all__ = ["SequentialContainer", "ParallelContainer", "RNN"]
+__all__ = ["SequentialContainer", "ParallelContainer"]
 
 
 class SequentialContainer(Module):
-    """Sequential container."""
+    """Sequential container module."""
 
     def __init__(self, layers: list[Module]) -> None:
         """Sequential container module.
@@ -42,49 +41,19 @@ class SequentialContainer(Module):
 
 
 class ParallelContainer(Module):
-    """Parallel container."""
+    """Parallel container module."""
 
     def __init__(self, layers: list[Module]) -> None:
+        """Parallel container module.
+
+        Parameters
+        ----------
+        layers : list[Module]
+            List of layers used in the container.
+            These layers are processed in parallel and their outputs are concatenated.
+        """
         super().__init__()
         self.sub_modules = layers
 
     def __call__(self, x: Tensor) -> Tensor:
         raise NotImplementedError()
-
-
-class RNN(SequentialContainer):
-    """Recurrent neural network model."""
-
-    def __init__(
-        self,
-        in_channels: int,
-        hidden_channels: int,
-        activation: str = "tanh",
-        num_layers: int = 1,
-        use_bias: bool = True,
-    ) -> None:
-        """Recurrent neural network model.
-
-        Parameters
-        ----------
-        in_channels : int
-            Number of input features.
-        hidden_channels: int
-            Number of hidden features.
-        activation: Module, optional
-            Activation function to be used in the hidden layer, by default Tanh().
-        num_layers: int, optional
-            Number of recurrent layers in the model, by default 1.
-        use_bias : bool, optional
-            Whether to use bias values, by default True.
-        """
-        modules = []
-        for i in range(num_layers):
-            if i == 0:
-                modules.append(Linear(in_channels, hidden_channels, use_bias=use_bias))
-            else:
-                modules.append(
-                    Linear(hidden_channels, hidden_channels, use_bias=use_bias)
-                )
-            modules.append(Recurrent(hidden_channels, activation, use_bias=use_bias))
-        super().__init__(modules)
