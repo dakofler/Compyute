6c05039ce1ada0562310ab649260f97934e38529
dakofler <dkofler@outlook.com>
2023-10-08 20:06:13 +0200

    refactored dataloader

--- walnut/nn/dataloaders.py
+++ walnut/nn/dataloaders.py
@@ -10,24 +10,24 @@ __all__ = ["DataLoader"]
 class DataLoader:
     """DataLoader base class."""
 
-    def __init__(self, X: Tensor, y: Tensor | None = None, batch_size: int = 1) -> None:
+    def __init__(self, x: Tensor, y: Tensor | None = None, batch_size: int = 1) -> None:
         """DataLoader base class.
 
         Parameters
         ----------
-        X : Tensor
+        x : Tensor
             Freature tensor.
         y : Tensor, optional
             Label tensor, by default None.
         batch_size : int, optional
             Size of returned batches, by default 1.
         """
-        self.X = X
+        self.x = x
         self.y = y
         self.batch_size = batch_size
 
     def __call__(self, shuffle: bool = True):
-        """Returns batched data.
+        """Returns data in a batched form.
 
         Parameters
         ----------
@@ -39,30 +39,28 @@ class DataLoader:
         tuple[Tensor, Tensor]
             Batch of features and labels.
         """
-        n = len(self.X)
+        n = len(self.x)
         n_steps = len(self)
         b = min(self.batch_size, n)
-        n_regular = n_steps * b
+        n_trunc = n_steps * b
 
         if shuffle:
             indices = tu.random_permutation(n)
-            self.X = self.X[indices]
+            self.x = self.x[indices]
             if self.y is not None:
                 self.y = self.y[indices]
 
-        # if labels are provided
-        if self.y is not None:
-            for i in range(n_steps):
-                yield (self.X[i * b : (i + 1) * b], self.y[i * b : (i + 1) * b])
-            # yield remaining samples, if there are any
-            if n_regular < len(self.X):
-                yield (self.X[n_regular:], self.y[n_regular:])
-        else:
-            for i in range(n_steps):
-                yield (self.X[i * b : (i + 1) * b], None)
-            # yield remaining samples, if there are any
-            if n_regular < len(self.X):
-                yield (self.X[n_regular:], None)
+        # yield batches
+        for i in range(n_steps):
+            x = self.x[i * b : (i + 1) * b]
+            y = self.y[i * b : (i + 1) * b] if self.y is not None else None
+            yield (x, y)
+
+        # yield remaining samples, if there are any
+        if n_trunc < n:
+            x_remain = self.x[n_trunc:]
+            y_remain = self.y[n_trunc:] if self.y is not None else None
+            yield (x_remain, y_remain)
 
     def __len__(self) -> int:
-        return max(1, len(self.X) // self.batch_size)
+        return max(1, len(self.x) // self.batch_size)
